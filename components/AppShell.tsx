5ba87092dc2366da84debd36698b0ea74571ca8a
austin stickley <astickleyid@gmail.com>
2026-05-03 08:08:58 -0400

    Round 6: confirm dialog system + template picker + focus refresh fixes

--- components/AppShell.tsx
+++ components/AppShell.tsx
@@ -3,6 +3,7 @@
 import { useState, createContext, useContext } from 'react';
 import Sidebar from './Sidebar';
 import { ToastProvider } from './Toast';
+import { ConfirmProvider } from './Confirm';
 
 const ShellContext = createContext({ openSidebar: () => {} });
 
@@ -12,13 +13,15 @@ export default function AppShell({ businessName, children }: { businessName: str
   const [open, setOpen] = useState(false);
 
   return (
-    <ToastProvider>
-      <ShellContext.Provider value={{ openSidebar: () => setOpen(true) }}>
-        <div className="flex min-h-screen">
-          <Sidebar businessName={businessName} open={open} onClose={() => setOpen(false)} />
-          <main className="flex-1 min-w-0 overflow-x-hidden">{children}</main>
-        </div>
-      </ShellContext.Provider>
-    </ToastProvider>
+    <ConfirmProvider>
+      <ToastProvider>
+        <ShellContext.Provider value={{ openSidebar: () => setOpen(true) }}>
+          <div className="flex min-h-screen">
+            <Sidebar businessName={businessName} open={open} onClose={() => setOpen(false)} />
+            <main className="flex-1 min-w-0 overflow-x-hidden">{children}</main>
+          </div>
+        </ShellContext.Provider>
+      </ToastProvider>
+    </ConfirmProvider>
   );
 }
